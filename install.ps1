99a234310fd58c75a8b6bef7fa0c4a958ac4fc0e
Brandon <pacewicbrandon@gmail.com>
2023-03-22 10:35:05 -0700

    Converted to 4 spaces

--- install.ps1
+++ install.ps1
@@ -8,22 +8,22 @@ $paths = [Environment]::GetEnvironmentVariable("PATH", "Machine")
 Write-Host "Linking Python files in child directories to the PATH environment variable..."
 
 foreach ($d in $dir) {
-  if ($paths -contains $d) {
-    Write-Error "$d is already in the PATH environment variable."
-    continue
-  }
-  
-  $paths = "$d;$paths"
-  Write-Host "Adding $d to PATH environment variable."
+    if ($paths -contains $d) {
+        Write-Error "$d is already in the PATH environment variable."
+        continue
+    }
+    
+    $paths = "$d;$paths"
+    Write-Host "Adding $d to PATH environment variable."
 
-  $files = Get-ChildItem -Path $d -Filter *.py
-  
-  foreach ($f in $files) {
-    $wrapper = "$d\$($f.BaseName).cmd"
-    $command = "@echo off`npython3 `"$($f.FullName)`" %*"
-    Set-Content -Path $wrapper -Value $command
-    Write-Host "Created wrapper script for $($f.Name)."
-  }
+    $files = Get-ChildItem -Path $d -Filter *.py
+    
+    foreach ($f in $files) {
+        $wrapper = "$d\$($f.BaseName).cmd"
+        $command = "@echo off`npython3 `"$($f.FullName)`" %*"
+        Set-Content -Path $wrapper -Value $command
+        Write-Host "Created wrapper script for $($f.Name)."
+    }
 }
 
 [Environment]::SetEnvironmentVariable("PATH", $paths, "Machine")
